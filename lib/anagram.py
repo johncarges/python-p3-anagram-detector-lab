631a112f8ecdd81522bc89ffec11895cb2d5915a
John Carges <johncarges@gmail.com>
2023-08-22 14:07:26 -0400

    adds anagram functionality

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list_of_words):
+        def split_word(word):
+            return sorted([letter for letter in word])
+        return [word for word in list_of_words if split_word(word) == split_word(self.word)]
+    pass
